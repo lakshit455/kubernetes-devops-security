8dc525c4e72767da84e86ba31d98b2bb076d0e11
lakshit455 <89823179+lakshit455@users.noreply.github.com>
2022-05-26 11:58:43 +0530

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PORT=$(kubectl -n default get svc ${devsecops-svcc} -o json | jq .spec.ports[].nodePort)
+#PORT=$(kubectl -n default get svc ${devsecops-svcc} -o json | jq .spec.ports[].nodePort)
 
 # first run this
 chmod 777 $(pwd)
